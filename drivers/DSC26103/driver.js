771e242cbf2cddf37f26f70bf773262d67b9da86
Niels <vrolijkegast@gmail.com>
2017-12-23 22:04:09 +0100

    Added learnmode image, minor fixes

--- drivers/DSC26103/driver.js
+++ drivers/DSC26103/driver.js
@@ -5,29 +5,31 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
-		onoff: [{
-			command_class: 'COMMAND_CLASS_SWITCH_BINARY',
-			command_get: 'SWITCH_BINARY_GET',
-			command_set: 'SWITCH_BINARY_SET',
-			command_set_parser: value => ({
-				'Switch Value': (value) ? 0xFF : 0x00,
-			}),
-			command_report: 'SWITCH_BINARY_REPORT',
-			command_report_parser: report => {
-				if (report && report.hasOwnProperty('Value')) {
-					if (report.Value === 'on/enable') return true;
-					else if (report.Value === 'off/disable') return false;
+		onoff: [
+			{
+				command_class: 'COMMAND_CLASS_SWITCH_BINARY',
+				command_get: 'SWITCH_BINARY_GET',
+				command_set: 'SWITCH_BINARY_SET',
+				command_set_parser: value => ({
+					'Switch Value': (value) ? 0xFF : 0x00,
+				}),
+				command_report: 'SWITCH_BINARY_REPORT',
+				command_report_parser: report => {
+					if (report && report.hasOwnProperty('Value')) {
+						return (report.Value === 'on/enable');
+					}
+					return null;
 				}
-				return null;
-			},
-		}, {
-			command_class: 'COMMAND_CLASS_BASIC',
-			command_report: 'BASIC_SET',
-			command_report_parser: report => {
-				if (report && report.hasOwnProperty('Value')) return report.Value > 0;
-				return null;
-			},
-		}],
+			}, {
+				command_class: 'COMMAND_CLASS_BASIC',
+				command_report: 'BASIC_SET',
+				command_report_parser: report => {
+					if (report && report.hasOwnProperty('Value')) return report.Value > 0;
+					return null;
+				}, 
+			}
+		],
+
 	},
 	settings: {
 		blinking: {
@@ -53,10 +55,6 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		device_tag: {
 			index: 254,
 			size: 2,
-		},
-		config_reset: {
-			index: 255,
-			size: 1,
 		}
 	}
 });
